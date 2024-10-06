7fc14187064ff58e9c2a6e6e11c89b70b0a82669
Md.Rasal <160160726+mdraselses10@users.noreply.github.com>
2024-10-07 02:34:37 +0800

    Add files via upload

---
+++ problem set-5.py
@@ -0,0 +1,11 @@
+s = input("Enter a string: ")
+
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+
+print(f"Number of vowels: {count_vowels(s)}")
