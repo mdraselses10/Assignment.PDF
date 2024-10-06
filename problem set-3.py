cddbdb1ca27c06ca9e1a06d0d37792d74be71a1a
Md.Rasal <160160726+mdraselses10@users.noreply.github.com>
2024-10-07 02:34:00 +0800

    Add files via upload

---
+++ problem set-3.py
@@ -0,0 +1,10 @@
+s = input("Enter a string: ")
+
+def count_digits(s):
+    digit_count = 0
+    for char in s:
+        if char.isdigit():
+            digit_count += 1
+    return digit_count
+
+print(f"Total digits are: {count_digits(s)}")
